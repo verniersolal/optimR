313409c178d6fe98f502b2ad0fd185f536f2d71e
svernier <verniersolal@gmail.com>
2019-04-11 01:24:00 +0200

    profile_page

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,11 @@
 $(document).ready(function () {
     $('.sidenav').sidenav();
 });
+
+$(document).ready(function () {
+    $('select').formSelect();
+});
+
+$('.saveInput').on('change',function () {
+   $('#saveInfoBtn').removeClass("disabled");
+});
