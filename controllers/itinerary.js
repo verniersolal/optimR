cf27a2187750b70596c392b2cd10c815e6220c6b
svernier <verniersolal@gmail.com>
2019-04-12 02:40:26 +0200

    firt refactor itinerary view

--- controllers/itinerary.js
+++ controllers/itinerary.js
@@ -5,15 +5,8 @@ const trajet = require('../models/index').trajet;
 
 exports.getItinerary = function (req, res) {
     if (req.method === 'GET') {
-        return res.render('itinerary.ejs', {
-            data: JSON.stringify('data'), user: req.session.user,
-            logged: true
-        });
-    }
-}
-exports.getItinerary = function(req, res){
-    if(req.method==='GET'){
-        return res.render('itinerary.ejs', { data: JSON.stringify('data')});
+        return res.render('itinerary.ejs',
+            {data: JSON.stringify('data'), user: req.session.user, logged: true});
     }
     request({
         url: 'https://geocoder.api.here.com/6.2/geocode.json',
@@ -47,7 +40,6 @@ exports.getItinerary = function(req, res){
                 return res.status(400).json({'message': err2, 'error': true})
             }
             let Position2 = JSON.parse(body2).Response.View[0].Result[0].Location.DisplayPosition;
-            res.render('itinerary.ejs', { pointA: Position1, pointB: Position2});
 
             res.render('itinerary.ejs',
                 {
@@ -60,9 +52,9 @@ exports.getItinerary = function(req, res){
     });
 
 };
-exports.import = function(req,res){
+exports.import = function (req, res) {
     let data = [];
-    var sumDistance =0;
+    var sumDistance = 0;
     var sumDuration = 0;
     var sumPrice = 0;
     var sumCo2 = 0.0;
@@ -74,9 +66,9 @@ exports.import = function(req,res){
     var first_lgta = req.body.trajets[0].lgta;
     var first_lata = req.body.trajets[0].lata;
     var compteur = 0;
-    req.body.trajets.forEach((t, i)=>{
-        if((t.transport_type !== current_transport)) {// nouveau step
-            if(step===1){
+    req.body.trajets.forEach((t, i) => {
+        if ((t.transport_type !== current_transport)) {// nouveau step
+            if (step === 1) {
                 data.push({
                     distance: sumDistance,
                     transport_type: current_transport,
@@ -91,7 +83,7 @@ exports.import = function(req,res){
                     departure_time: t.departure_time,
                     step: step,
                 });
-            }else {
+            } else {
                 data.push({
                     distance: sumDistance,
                     transport_type: current_transport,
@@ -101,8 +93,8 @@ exports.import = function(req,res){
                     no2: sumNo2,
                     pm10: sumPm10,
                     price: sumPrice,
-                    lgta: req.body.trajets[i - compteur-1].lgta,
-                    lata: req.body.trajets[i - compteur-1].lata,
+                    lgta: req.body.trajets[i - compteur - 1].lgta,
+                    lata: req.body.trajets[i - compteur - 1].lata,
                     departure_time: t.departure_time,
                     step: step,
                 });
@@ -114,23 +106,23 @@ exports.import = function(req,res){
             sumDistance = parseInt(t.distance);
             sumDuration = parseInt(t.duration);
             sumCo2 = parseFloat(t.co2);
-            sumCalories =  parseFloat(t.calories);
-            sumNo2 =  parseFloat(t.no2);
+            sumCalories = parseFloat(t.calories);
+            sumNo2 = parseFloat(t.no2);
             sumPm10 = parseFloat(t.pm10);
-            sumPrice =  parseFloat(t.price);
-        } else{ // aliment le step
+            sumPrice = parseFloat(t.price);
+        } else { // aliment le step
             compteur++;
-            sumDistance = sumDistance+ parseInt(t.distance);
-            sumDuration = sumDuration+ parseInt(t.duration);
-            sumCo2 = sumCo2+ parseFloat(t.co2);
-            sumCalories = sumCalories+ parseFloat(t.calories);
-            sumNo2 = sumNo2+ parseFloat(t.no2);
-            sumPm10 = sumPm10+ parseFloat(t.pm10);
-            sumPrice = sumPrice+ parseFloat(t.price);
+            sumDistance = sumDistance + parseInt(t.distance);
+            sumDuration = sumDuration + parseInt(t.duration);
+            sumCo2 = sumCo2 + parseFloat(t.co2);
+            sumCalories = sumCalories + parseFloat(t.calories);
+            sumNo2 = sumNo2 + parseFloat(t.no2);
+            sumPm10 = sumPm10 + parseFloat(t.pm10);
+            sumPrice = sumPrice + parseFloat(t.price);
         }
 
     });
-    if(req.body.trajets[req.body.trajets.length-1].transport_type === current_transport){
+    if (req.body.trajets[req.body.trajets.length - 1].transport_type === current_transport) {
         data.push({
             distance: sumDistance,
             transport_type: current_transport,
@@ -140,12 +132,12 @@ exports.import = function(req,res){
             no2: sumNo2,
             pm10: sumPm10,
             price: sumPrice,
-            lgta: req.body.trajets[req.body.trajets.length-1-compteur].lgta,
-            lata: req.body.trajets[req.body.trajets.length-1-compteur].lata,
-            departure_time: req.body.trajets[req.body.trajets.length-1].departure_time,
+            lgta: req.body.trajets[req.body.trajets.length - 1 - compteur].lgta,
+            lata: req.body.trajets[req.body.trajets.length - 1 - compteur].lata,
+            departure_time: req.body.trajets[req.body.trajets.length - 1].departure_time,
             step: step,
         })
-    }else{
+    } else {
 
         data.push({
             distance: sumDistance,
@@ -156,33 +148,33 @@ exports.import = function(req,res){
             no2: sumNo2,
             pm10: sumPm10,
             price: sumPrice,
-            lgta: req.body.trajets[req.body.trajets.length-1-compteur].lgta,
-            lata: req.body.trajets[req.body.trajets.length-1-compteur].lata,
-            departure_time: req.body.trajets[req.body.trajets.length-1].departure_time,
+            lgta: req.body.trajets[req.body.trajets.length - 1 - compteur].lgta,
+            lata: req.body.trajets[req.body.trajets.length - 1 - compteur].lata,
+            departure_time: req.body.trajets[req.body.trajets.length - 1].departure_time,
             step: step,
         });
 
         step++;
         data.push({
-            distance: req.body.trajets[req.body.trajets.length-1].distance,
-            transport_type: req.body.trajets[req.body.trajets.length-1].transport_type,
-            duration: req.body.trajets[req.body.trajets.length-1].duration,
-            co2: req.body.trajets[req.body.trajets.length-1].co2,
-            calories: req.body.trajets[req.body.trajets.length-1].calories,
-            no2: req.body.trajets[req.body.trajets.length-1].no2,
-            pm10: req.body.trajets[req.body.trajets.length-1].pm10,
-            price: req.body.trajets[req.body.trajets.length-1].price,
-            lgta: req.body.trajets[req.body.trajets.length-1].lgta,
-            lata: req.body.trajets[req.body.trajets.length-1].lata,
-            departure_time: req.body.trajets[req.body.trajets.length-1].departure_time,
+            distance: req.body.trajets[req.body.trajets.length - 1].distance,
+            transport_type: req.body.trajets[req.body.trajets.length - 1].transport_type,
+            duration: req.body.trajets[req.body.trajets.length - 1].duration,
+            co2: req.body.trajets[req.body.trajets.length - 1].co2,
+            calories: req.body.trajets[req.body.trajets.length - 1].calories,
+            no2: req.body.trajets[req.body.trajets.length - 1].no2,
+            pm10: req.body.trajets[req.body.trajets.length - 1].pm10,
+            price: req.body.trajets[req.body.trajets.length - 1].price,
+            lgta: req.body.trajets[req.body.trajets.length - 1].lgta,
+            lata: req.body.trajets[req.body.trajets.length - 1].lata,
+            departure_time: req.body.trajets[req.body.trajets.length - 1].departure_time,
             step: step,
         });
     }
-    for(var i =0; i+1 < data.length;i++){
-        data[i]['lgtb'] = data[i+1].lgta;
-        data[i]['latb'] = data[i+1].lata;
+    for (var i = 0; i + 1 < data.length; i++) {
+        data[i]['lgtb'] = data[i + 1].lgta;
+        data[i]['latb'] = data[i + 1].lata;
     }
-    for(let i=0; i< data.length; i++) {
+    for (let i = 0; i < data.length; i++) {
         var score;
         switch (data[i]['transport_type']) {
             case 'car':
@@ -206,15 +198,15 @@ exports.import = function(req,res){
         }
         data[i]['score'] = Math.floor(score);
     }
-    trajet.max('trajetid').then(maxValue =>{
-        if(isNaN(maxValue)) {
+    trajet.max('trajetid').then(maxValue => {
+        if (isNaN(maxValue)) {
             maxValue = 0;
-            for(var i =0; i<data.length;i++) {
+            for (var i = 0; i < data.length; i++) {
                 data[i]['trajetid'] = 0;
             }
 
-        }else{
-            for(var i =0; i<data.length;i++) {
+        } else {
+            for (var i = 0; i < data.length; i++) {
                 data[i]['trajetid'] = maxValue + 1;
             }
         }
@@ -229,6 +221,6 @@ exports.import = function(req,res){
     });
 };
 
-exports.getData = function(req,res){
+exports.getData = function (req, res) {
     //recuperer le dernier trajet et retourner sous forme de json
 }
